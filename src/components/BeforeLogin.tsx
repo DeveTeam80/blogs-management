e4c96d6a75b7cfa838842a53f21e2fd059730741
qusai-rang <qusairang86@gmail.com>
2026-05-28 14:17:13 +0530

    resend email user change

--- src/components/BeforeLogin.tsx
+++ src/components/BeforeLogin.tsx
@@ -25,35 +25,6 @@ export default function BeforeLogin() {
           display: none !important;
         }
       `}</style>
-
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginBottom: '8px',
-          fontSize: '13px',
-          color: '#888',
-        }}
-      >
-        <span>
-          New user?{' '}
-          <a
-            href="/signup"
-            style={{
-              color: '#1a1a1a',
-              fontWeight: '600',
-              textDecoration: 'none',
-              borderBottom: '1px solid #1a1a1a',
-            }}
-          >
-            Sign up
-          </a>
-        </span>
-        <a href="/admin/forgot" style={{ color: '#888', textDecoration: 'none' }}>
-          Forgot password?
-        </a>
-      </div>
     </>
   )
 }
